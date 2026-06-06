d63c81a6b85efb0bf79e5d04958220abf30d64f9
[dyad] <marketinggpetnterprise@gmail.com>
2026-06-06 14:59:52 +0530

    [dyad] Updated the dark mode toggle button to make the Sun and Moon icons yellow when active - wrote 1 file(s)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -142,8 +142,8 @@ export const Navbar = () => {
             className="h-10 w-10 hover:bg-primary/5 transition-colors rounded-full"
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
           >
-            <Sun className="h-5 w-5 text-gray-700 dark:text-gray-200 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-5 w-5 text-gray-700 dark:text-gray-200 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className="h-5 w-5 text-yellow-500 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-5 w-5 text-yellow-400 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           </Button>
 
           {/* Mobile Search Toggle */}
